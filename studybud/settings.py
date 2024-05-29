9aa9ba9a58ff24f85f59a967a21e874d753e714a
nauman.sameja <nauman.samejaa@gmail.com>
2024-05-29 19:23:49 +0500

    removed sensitive information

--- studybud/settings.py
+++ studybud/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nauman.samejaa@gmail.com'
-EMAIL_HOST_PASSWORD = 'jzyf zuuw fsgf klpd'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'your_default_from_email_address'
 
